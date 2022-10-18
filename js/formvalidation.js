03c7f102378a1359ed82c768371f11ddf34e909b
chiranjeev <chiranjeevmohanty6969@gmail.com>
2022-10-18 20:49:23 +0530

    form corrections

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -17,6 +17,13 @@ function validateForm() {
         document.querySelector(".name-err-icon").style.display = "block";
         document.querySelector(".name-suc-icon").style.display = "none";
         document.querySelector(".name-err").innerHTML = "The field is blank";
+        document.querySelector(".tel-err-icon").style.display = "none";
+        // document.querySelector(".tel-suc-icon").style.display = "none";
+        document.querySelector(".email-err-icon").style.display = "none";
+        // document.querySelector(".email-suc-icon").style.display = "none";
+        document.querySelector(".email-err").innerHTML = "";
+        document.querySelector(".tel-err").innerHTML = "";
+        document.querySelector(".message-error").innerHTML = "";
         namefield.focus();
         return false;
     } else {
@@ -25,73 +32,113 @@ function validateForm() {
             document.querySelector(".name-err-icon").style.display = "block";
             document.querySelector(".name-suc-icon").style.display = "none";
             document.querySelector(".name-err").innerHTML = "Please enter valid data.";
+            document.querySelector(".tel-err-icon").style.display = "none";
+            document.querySelector(".email-err-icon").style.display = "none";
+            document.querySelector(".email-err").innerHTML = "";
+            document.querySelector(".tel-err").innerHTML = "";
+            document.querySelector(".message-error").innerHTML = "";
             namefield.focus();
             return false;
         } else {
             document.querySelector(".name-err-icon").style.display = "none";
             document.querySelector(".name-suc-icon").style.display = "block";
             document.querySelector(".name-err").innerHTML = "";
+            document.querySelector(".tel-err-icon").style.display = "none";
+            document.querySelector(".email-err-icon").style.display = "none";
+            document.querySelector(".email-err").innerHTML = "";
+            document.querySelector(".tel-err").innerHTML = "";
+            document.querySelector(".message-error").innerHTML = "";
             namefield.blur();
             NameError = false;
         }
     }
 
     if (phoneNumber == null || phoneNumber == "") {
+        document.querySelector(".name-err-icon").style.display = "none";
+        document.querySelector(".name-err").innerHTML = "";
         document.querySelector(".tel-err-icon").style.display = "block";
-        document.querySelector(".tel-suc-icon").style.display = "none"
+        document.querySelector(".tel-suc-icon").style.display = "none";
         document.querySelector(".tel-err").innerHTML = "The field is blank";
+        document.querySelector(".email-err-icon").style.display = "none";
+        document.querySelector(".email-err").innerHTML = "";
+        document.querySelector(".message-error").innerHTML = "";
         numberField.focus();
         return false;
     } else {
         regex = /^[6-9]\d{9}$/;
         if (regex.test(phoneNumber) === false) {
+            document.querySelector(".name-err-icon").style.display = "none";
+            document.querySelector(".name-err").innerHTML = "";
             document.querySelector(".tel-err-icon").style.display = "block";
             document.querySelector(".tel-suc-icon").style.display = "none";
             document.querySelector(".tel-err").innerHTML = "Please enter valid number";
+            document.querySelector(".email-err-icon").style.display = "none";
+            document.querySelector(".email-err").innerHTML = "";
+            document.querySelector(".message-error").innerHTML = "";
             numberField.focus();
             return false;
-        }else{
+        } else {
+            document.querySelector(".name-err-icon").style.display = "none";
+            document.querySelector(".name-err").innerHTML = "";
             document.querySelector(".tel-err-icon").style.display = "none";
             document.querySelector(".tel-suc-icon").style.display = "block";
             document.querySelector(".tel-err").innerHTML = "";
+            document.querySelector(".email-err-icon").style.display = "none";
+            document.querySelector(".email-err").innerHTML = ""; 
+            document.querySelector(".message-error").innerHTML = "";
             numberField.blur();
             telError = false;
         }
     }
-    if(emailId == null || emailId == ""){
+    if (emailId == null || emailId == "") {
+        document.querySelector(".name-err-icon").style.display = "none";
+        document.querySelector(".name-err").innerHTML = "";
+        document.querySelector(".tel-err-icon").style.display = "none";
+        document.querySelector(".tel-err").innerHTML = "";
         document.querySelector(".email-err-icon").style.display = "block";
         document.querySelector(".email-suc-icon").style.display = "none";
         document.querySelector(".email-err").innerHTML = "The field is blank";
+        document.querySelector(".message-error").innerHTML = "";
         emailField.focus();
         return false;
-    }else{
+    } else {
         regex = /^\S+@\S+\.\S+$/;
         if (regex.test(emailId) === false) {
+            document.querySelector(".name-err-icon").style.display = "none";
+            document.querySelector(".name-err").innerHTML = "";
+            document.querySelector(".tel-err-icon").style.display = "none";
+            document.querySelector(".tel-err").innerHTML = "";
             document.querySelector(".email-err-icon").style.display = "block";
             document.querySelector(".email-suc-icon").style.display = "none";
-            document.querySelector(".email-err").innerHTML = "Please enter valid email";
+            document.querySelector(".email-err").innerHTML = "Please enter valid data";
+            document.querySelector(".message-error").innerHTML = "";
             emailField.focus();
             return false;
-        }else{
+        } else {
+            document.querySelector(".name-err-icon").style.display = "none";
+            document.querySelector(".name-err").innerHTML = "";
+            document.querySelector(".tel-err-icon").style.display = "none";
+            document.querySelector(".tel-err").innerHTML = "";
             document.querySelector(".email-err-icon").style.display = "none";
-            document.querySelector(".email-err").innerHTML = "";
             document.querySelector(".email-suc-icon").style.display = "block";
-            emailField.focus();
+            document.querySelector(".email-err").innerHTML = "";
+            document.querySelector(".message-error").innerHTML = "";
+            emailField.blur();
             emailError = false;
         }
     }
-    if(message == null || message == ""){
+    if (message == null || message == "") {
         messageField.focus();
         document.querySelector(".message-error").innerHTML = "The field is blank.";
         return false;
-    }else{
+    } else {
         document.querySelector(".message-error").innerHTML = "The field is blank.";
     }
 }
 
 clearButton = document.getElementById("clear");
-icons = document.querySelectorAll(".icon"); 
-clearButton.addEventListener("click", ()=>{
+icons = document.querySelectorAll(".icon");
+clearButton.addEventListener("click", () => {
     icons[0].style.display = "none";
     icons[1].style.display = "none";
     icons[2].style.display = "none";
